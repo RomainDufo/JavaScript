21171b8f999d16fb219176cd4766bf9878efb702
RomainDufo <118887058+RomainDufo@users.noreply.github.com>
2023-01-13 23:02:28 +0100

    Add files via upload

--- TPevenement.js
+++ TPevenement.js
@@ -1,5 +1,5 @@
 console.log("TP Evenement");
-
+/*
 //Fonction pour creer element//
 function createDom(element_name, text, parent) {
     const markup = document.createElement(element_name);
@@ -7,29 +7,19 @@ function createDom(element_name, text, parent) {
     parent.appendChild(markup);
     return markup;
 }
+let navbar = createDom("nav", "------NAVBAR-------", document.body);
+let ul = createDom("ul", "", navbar);
+let boutonForm = createDom("button", "Afficher Formulaire", document.body);
+let formulaire = createDom("form", "", document.body);
+let titreLabel = createDom("label", " Ecrire votre Titre: ", formulaire);
+let titreInput = createDom("input", "", formulaire);
+let paragLabel = createDom("label", " Ecrire vos paragraphes: ", formulaire);
+let paragInput = createDom("input", "", formulaire);
+let btnEditer = createDom("button", "Editer", formulaire);
+let divArticle = createDom("div", "", document.body);
 
 
-let navbar = createDom("nav", "----- navbar -------", document.body);
-let boutonFormulaire = createDom("button", "Afficher Formulaire", document.body);
 
-let titreInput;
-let paragInput;
-
-//Au clique du bouton "Afficher formulaire" il se passera ...//
-boutonFormulaire.addEventListener('click', () => {
-    let formulaire = createDom("form", "", document.body);
-    let titreLabel = createDom("label", " Ecrire votre Titre: ", formulaire);
-    titreInput = createDom("input", "", formulaire);
-    let paragLabel = createDom("label", " Ecrire vos paragraphes: ", formulaire);
-    paragInput = createDom("input", "", formulaire);
-    let btnEditer = createDom("button", "Editer", formulaire);
-    btnEditer.addEventListener('click', () => {
-        editerArticle();
-    })
-})
-
-
-/*
 function editerArticle() {
     let Div1 = createDom("div", "", document.body);
     createDom("h2", titreInput.value, Div1);
@@ -42,14 +32,105 @@ function editerNav() {
 
 }
 */
+/*boutonForm.style.display = 'none';
+
+if (titreInput.value.trim == "" || paragInput.value.trim == "") {
+        e.preventDefault();
+    }*/
+
+
+let navbar = document.createElement("nav");
+window.document.body.appendChild(navbar);
+navbar.textContent = "------NAVBAR-------";
+
+let ulNav = document.createElement("ul");
+navbar.appendChild(ulNav);
+
+let boutonForm = document.createElement("button");
+window.document.body.appendChild(boutonForm);
+boutonForm.textContent = "Afficher Formulaire";
+
+let formulaire = document.createElement("form");
+window.document.body.appendChild(formulaire);
+formulaire.hidden = true;
+
+let titreLabel = document.createElement("label");
+formulaire.appendChild(titreLabel);
+titreLabel.textContent = "Ecrire votre Titre: ";
+
+let titreInput = document.createElement("input");
+formulaire.appendChild(titreInput);
+titreInput.textContent = "";
+
+let paragLabel = document.createElement("label");
+formulaire.appendChild(paragLabel);
+paragLabel.textContent = "Ecrire vos paragraphes: ";
+
+let paragInput = document.createElement("input");
+formulaire.appendChild(paragInput);
+paragInput.textContent = "";
+
+let btnEditer = document.createElement("button");
+formulaire.appendChild(btnEditer);
+btnEditer.textContent = "Editer";
+
+let divArticle = document.createElement("div");
+window.document.body.appendChild(divArticle);
 
 
 
 
+boutonForm.addEventListener('click', function () {
+    if (formulaire.style.display == 'none') {
+        formulaire.style.display = 'block';
+    } else {
+        formulaire.style.display = 'none';
+    }
+});
+
+
+btnEditer.addEventListener('click', function () {
+    article.hidden = false;
+    newArticle()
+    newTitreNavbar()
+});
+
+
+let article = document.createElement("article");
+divArticle.appendChild(article);
+
+let titre = document.createElement("h2");
+article.appendChild(titre);
+
+let paragraphe = document.createElement("p");
+article.appendChild(paragraphe);
+
+let boutonEdit = document.createElement("button");
+article.appendChild(boutonEdit);
+boutonEdit.textContent = "Supprimer";
+
+article.hidden = true;
 
 
 
 
+function newArticle(event) {
+    titre.textContent = titreInput.value;
+    paragraphe.textContent = paragInput.value;
+}
 
+
+
+
+function newTitreNavbar() {
+    let li = document.createElement("li");
+    ulNav.appendChild(li);
+
+    let titreNavbar = document.createElement("a");
+    li.appendChild(titreNavbar);
+    titreNavbar.textContent = titreInput.value;
+
+    return false;
+}
 
 
